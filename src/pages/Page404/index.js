4e1fd98b518f7cad8f52ecd4c27691fbeb582299
Bruno Dornela <brunodornela@hotmail.com>
2020-08-04 18:24:45 -0300

    Stylishing 404 page

--- src/pages/Page404/index.js
+++ src/pages/Page404/index.js
@@ -1,15 +1,53 @@
 import React from 'react';
-import Button from '../../components/Menu/components/Button';
-import { Link } from 'react-router-dom';
+import Button from '../../components/Menu/components/Button'
+import Background from '../../assets/img/obliviate.gif'
+import Menu from '../../components/Menu';
+import styled from 'styled-components';
 
+const FullDiv = styled.div`
+  color: var(--white);
+  min-height: calc(100vh - var(--bodyPaddingTop));
+  background-size: cover;
+  background-image: ${`url(${Background})`};
+  background-position: center;
+  display:flex;
+  justify-items: center;
+`
+
+const TextDiv = styled.div`
+  background-color:"orange";
+  flex:1;
+  align-items:center;
+`
 function Page404() {
     return (
-      <div style={{color:"red", background: "#141414"}}> Error 404 - Page not found! :'( 
-        <br></br>
-        <Button as={Link} to="/" className="ButtonLink">
-            Voltar pra home
-        </Button>
-      </div>
+      <>
+      <Menu/>
+      <FullDiv style={{ backgroundColor:"orange"}}>
+        
+        <TextDiv style={{display:"flex", flexDirection:"column"}}>
+          
+          <div style={{flex:1}} ></div>
+          <h1 style={{flex:0, verticalAlign:"botto"}}>
+            - Obliviate!
+          </h1>
+          
+          <h3 style={{flex:0}}>
+            How you get in here?
+          </h3>
+          
+          <Button to="/" style={{flex:0}}>
+            Back to home
+          </Button>
+          
+          <h4 style={{flex:1}}>
+            Unfortunely, we cant find that page
+          </h4>
+        </TextDiv>
+
+      </FullDiv>
+      
+      </>
     )
   }
 
